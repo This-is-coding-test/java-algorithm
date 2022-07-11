9fa3adeb0fc0c3c16935e39313be52327101e0db
yoon <dudwls143@gmail.com>
2022-07-12 01:00:45 +0900

    보이는 학생 완료

---
+++ 윤영진/Section2/src/보이는_학생.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 보이는_학생 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 1;
+
+        for (int i = 1; i < N; i++) {
+            boolean check = true;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] <= arr[j]) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) answer++;
+
+        }
+
+        System.out.println(answer);
+
+    }
+}
