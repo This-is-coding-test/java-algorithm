ad6dea36f862c43d02d78713db7b0a287580aa9b
yoon <dudwls143@gmail.com>
2022-07-21 21:55:23 +0900

    연속 부분수열 완료

---
+++ 윤영진/Section3/src/연속_부분수열.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연속_부분수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = i; j < N; j++) {
+                sum += arr[j];
+                if (sum > M) break;
+                if (sum == M) result ++;
+
+            }
+        }
+        System.out.println(result);
+    }
+}
