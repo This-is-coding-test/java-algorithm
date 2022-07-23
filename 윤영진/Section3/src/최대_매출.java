840ec07321f731dace92aa02738540436f4d2949
yoon <dudwls143@gmail.com>
2022-07-24 01:18:23 +0900

    최대 매출 수정

--- 윤영진/Section3/src/최대_매출.java
+++ 윤영진/Section3/src/최대_매출.java
@@ -4,8 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 최대_매출 {
+//    public static void main(String[] args) throws IOException {
+//        // TODO 시간초과 날거같은데..
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int K = Integer.parseInt(st.nextToken());
+//
+//        int[] arr = new int[N];
+//        st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0; i <= N - K; i++) {
+//            int sum = 0;
+//            for (int j = i; j < i + K; j++) {
+//                sum += arr[j];
+//            }
+//
+//            max = Math.max(max, sum);
+//        }
+//
+//        System.out.println(max);
+//    }
+
+    // Sliding Window
+
     public static void main(String[] args) throws IOException {
-        // TODO 시간초과 날거같은데..
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
@@ -19,26 +47,17 @@ public class 최대_매출 {
         }
 
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i <= N - K; i++) {
-            int sum = 0;
-            for (int j = i; j < i + K; j++) {
-                sum += arr[j];
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
+        for (int i = K; i < N; i++) {
+            sum = sum + arr[i] - arr[i - K];
+            if (sum > max) {
+                max = sum;
             }
-
-            max = Math.max(max, sum);
         }
-
         System.out.println(max);
+
     }
-
-    // Sliding Window
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-
-
-        }
 }
