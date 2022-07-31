5eb81dea88631d593f14a6151a3e595d5d943df6
yoon <dudwls143@gmail.com>
2022-07-31 17:02:46 +0900

    모든 아나그램 찾기 수정

--- 윤영진/Section4/src/모든_아나그램_찾기.java
+++ 윤영진/Section4/src/모든_아나그램_찾기.java
@@ -10,44 +10,36 @@ public class 모든_아나그램_찾기 {
         String S = br.readLine();
         String T = br.readLine();
 
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
 
-
-        int result = 0;
-
-        for (int i = 0; i < S.length() - T.length() + 1; i++) {
-            boolean check = true;
-            Map<String, Integer> map = makeMap(T);
-            for (int j = i; j < T.length() + i; j++) {
-                String c = String.valueOf(S.charAt(j));
-                if (map.get(c) == null) {
-                    check = false;
-                    break;
-                }else {
-                    map.put(c, map.get(c) - 1);
-                    if (map.get(c) == 0) {
-                        map.remove(c);
-                    }
-                }
-
-            }
-            if (check) {
-                result++;
-            }
+        for (Character c : T.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
         }
-        System.out.println(result);
-    }
-    public static Map<String, Integer> makeMap(String T) {
-        Map<String, Integer> map = new HashMap<>();
+        int lt = 0;
+        int rt = T.length() - 1;
 
-        for (int i = 0; i < T.length(); i++) {
-            String c = String.valueOf(T.charAt(i));
-            if (map.get(c) == null) {
-                map.put(c, 1);
-            }else {
-                map.put(c, map.get(c) + 1);
-            }
+        for (int i = 0; i < rt; i++) {
+            map2.put(S.charAt(i), map2.getOrDefault(S.charAt(i), 0) + 1);
         }
-        return map;
+        int answer = 0;
+        for (int i = rt; i < S.length(); i++) {
+            map2.put(S.charAt(i), map2.getOrDefault(S.charAt(i), 0) + 1);
+
+            // answer을 통해 두 HashMap 비교가능
+            if (map1.equals(map2)) {
+                answer++;
+            }
+            map2.put(S.charAt(lt), map2.get(S.charAt(lt)) - 1);
+            if (map2.get(S.charAt(lt)) == 0) {
+                map2.remove(S.charAt(lt));
+            }
+            lt++;
+        }
+
+        System.out.println(answer);
 
     }
+
+
 }
