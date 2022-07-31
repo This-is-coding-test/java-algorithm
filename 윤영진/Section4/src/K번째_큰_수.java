517cbb1d66adc8b4bfb850d713e00e3b9ef8c321
yoon <dudwls143@gmail.com>
2022-07-31 17:11:58 +0900

    K번째 큰 수 treeset 추가

--- 윤영진/Section4/src/K번째_큰_수.java
+++ 윤영진/Section4/src/K번째_큰_수.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class K번째_큰_수 {
     public static void main(String[] args) throws IOException {
@@ -21,22 +17,29 @@ public class K번째_큰_수 {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        List<Integer> list = new ArrayList<>();
+        // TreeSet은 중복 제거, 자동 정렬
+        TreeSet<Integer> ts = new TreeSet<>(Collections.reverseOrder());
 
         for (int i = 0; i < N - 2; i++) {
             for (int j = i + 1; j < N - 1; j++) {
                 for (int k = j + 1; k < N; k++) {
-                    list.add(arr[i] + arr[j] + arr[k]);
+                    ts.add(arr[i] + arr[j] + arr[k]);
                 }
             }
         }
-
-        List<Integer> collect = list.stream().distinct().sorted(Comparator.comparingInt(o -> -o)).collect(Collectors.toList());
-        if (K > collect.size()) {
+        if (ts.size() < K) {
             System.out.println(-1);
             return;
         }
-        System.out.println(collect.get(K - 1));
+
+
+        int cnt = 0;
+        for (int x : ts) {
+            cnt++;
+            if (cnt == K) {
+                System.out.println(x);
+            }
+        }
 
 
     }
