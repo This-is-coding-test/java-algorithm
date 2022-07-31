96299f5320a70d6a1e2ab542abac7bca4a9c711b
yoon <dudwls143@gmail.com>
2022-07-31 18:01:10 +0900

    교육과정 설계 수정

--- 윤영진/Section5/src/교육과정_설계.java
+++ 윤영진/Section5/src/교육과정_설계.java
@@ -4,23 +4,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 교육과정_설계 {
+    //    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String st1 = br.readLine();
+//        String st2 = br.readLine();
+//
+//        Set<Character> set = new HashSet<>();
+//
+//        for (Character x : st1.toCharArray()) {
+//            set.add(x);
+//        }
+//        String st3 = "";
+//        for (int i = 0; i < st2.length(); i++) {
+//            if (set.contains(st2.charAt(i))) {
+//                st3 += st2.charAt(i);
+//            }
+//        }
+//        System.out.print(st1.equals(st3) ? "YES" : "NO");
+//
+//    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String st1 = br.readLine();
         String st2 = br.readLine();
 
-        Set<Character> set = new HashSet<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        for (Character x : st1.toCharArray()) {
-            set.add(x);
+        for (Character c : st1.toCharArray()) {
+            queue.offer(c);
         }
-        String st3 = "";
-        for (int i = 0; i < st2.length(); i++) {
-            if (set.contains(st2.charAt(i))) {
-                st3 += st2.charAt(i);
+        for (Character c : st2.toCharArray()) {
+            if (queue.contains(c)) {
+                if (!queue.poll().equals(c)){
+                    System.out.println("NO");
+                    return;
+                }
             }
         }
-        System.out.print(st1.equals(st3) ? "YES" : "NO");
+
+        System.out.println(queue.isEmpty() ? "YES": "NO");
 
     }
 }
