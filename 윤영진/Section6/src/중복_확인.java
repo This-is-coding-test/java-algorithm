cbab9f51863de30cf6956910d2be9952bd7f0e16
yoon <dudwls143@gmail.com>
2022-08-05 01:39:37 +0900

    중복 확인 완료

--- 윤영진/Section6/src/중복_확인.java
+++ 윤영진/Section6/src/중복_확인.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class 중복_확인 {
@@ -11,15 +9,13 @@ public class 중복_확인 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
-        List<Integer> collect = list.stream().distinct().collect(Collectors.toList());
-
-        System.out.println(list.size() == collect.size() ? "U" : "D");
+        System.out.println(set.size() == N ? "U" : "D");
     }
 }
