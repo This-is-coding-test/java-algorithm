b578fed1e373f4cd52dbc0b15522fae26ab638e4
yoon <dudwls143@gmail.com>
2022-08-05 01:38:04 +0900

    feat: Last Recently Used, 중복 확인 완료

---
+++ 윤영진/Section6/src/Last_Recently_Used.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Last_Recently_Used {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int[] cache = new int[S];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < S; i++) {
+            cache[i] = 0;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cur = arr[i];
+            boolean check = true;
+
+            for (int j = 0; j < S; j++) {
+
+                // Hit
+                if (cur == cache[j]) {
+                    check = false;
+                    if (j != 0) {
+                        for (int k = j; k > 0; k--) {
+                            cache[k] = cache[k - 1];
+                        }
+                    }
+                }
+            }
+            // Miss
+            if (check) {
+                for (int j = S - 1; j > 0; j--) {
+                    cache[j] = cache[j - 1];
+                }
+            }
+            cache[0] = cur;
+        }
+
+        for (int i = 0; i < S; i++) {
+            System.out.print(cache[i] + " ");
+        }
+
+    }
+
+}
---
+++ 윤영진/Section6/src/중복_확인.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class 중복_확인 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        List<Integer> collect = list.stream().distinct().collect(Collectors.toList());
+
+        System.out.println(list.size() == collect.size() ? "U" : "D");
+    }
+}
