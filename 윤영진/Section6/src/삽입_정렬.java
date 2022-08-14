a91c4c77a15852c3f8a487b29d2e1c0cee030388
yoon <dudwls143@gmail.com>
2022-08-14 20:27:56 +0900

    삽입 정렬 수정

--- 윤영진/Section6/src/삽입_정렬.java
+++ 윤영진/Section6/src/삽입_정렬.java
@@ -16,14 +16,16 @@ public class 삽입_정렬 {
         }
 
         for (int i = 1; i < N; i++) {
-
-            for (int j = i; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    swap(arr, j, j-1);
-                } else {
+            int tmp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp) {
+                    arr[j + 1] = arr[j];
+                }else {
                     break;
                 }
             }
+            arr[j + 1] = tmp;
 
         }
 
