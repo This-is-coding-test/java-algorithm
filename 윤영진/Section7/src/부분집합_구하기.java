6a3c961475eb56092bab9cb5560c13320c900b02
yoon <dudwls143@gmail.com>
2022-08-18 21:43:11 +0900

    부분집합 구하기 완료

---
+++ 윤영진/Section7/src/부분집합_구하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부분집합_구하기 {
+
+    public static boolean[] check;
+    public static int N;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        check = new boolean[N + 1];
+        DFS(1);
+
+    }
+
+    private static void DFS(int n) {
+
+        if (n == N + 1) {
+            for (int i = 1; i <= N; i++) {
+                if (check[i]) System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }else {
+            check[n] = true;
+            DFS(n + 1);
+            check[n] = false;
+            DFS(n + 1);
+        }
+
+    }
+
+
+
+}
