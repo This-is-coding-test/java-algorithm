04a10e0ef62f555d8ad84182e6c160a176945357
yoon <dudwls143@gmail.com>
2022-08-18 21:54:43 +0900

    부분집합 그리기 완료

--- 윤영진/Section7/src/부분집합_구하기.java
+++ 윤영진/Section7/src/부분집합_구하기.java
@@ -19,11 +19,15 @@ public class 부분집합_구하기 {
     private static void DFS(int n) {
 
         if (n == N + 1) {
+            StringBuilder sb = new StringBuilder();
             for (int i = 1; i <= N; i++) {
-                if (check[i]) System.out.print(i + " ");
+                if (check[i]) {
+                    sb.append(i).append(" ");
+                }
             }
-            System.out.println();
-            return;
+            if (sb.length() != 0) {
+                System.out.println(sb);
+            }
         }else {
             check[n] = true;
             DFS(n + 1);
