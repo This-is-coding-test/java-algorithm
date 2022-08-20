16113dcc23cebf01acbf814d8b25c00c4c72c0bf
yoon <dudwls143@gmail.com>
2022-08-20 22:23:32 +0900

    경로 탐색 인접행렬 완료

--- 윤영진/Section7/src/경로_탐색_인접리스트.java
+++ 윤영진/Section7/src/경로_탐색_인접리스트.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class 경로_탐색_인접행렬 {
+public class 경로_탐색_인접리스트 {
+    // 정점의 개수가 많은 경우에는 인접행렬은 굉장히 비효율적이다.
+    // 따라서, 정점의 개수가 많은 경우에는 인접리스트로 풀자.
 
-    static int[][] graph;
-    static boolean[] check;
+    static ArrayList<ArrayList<Integer>> listGraph = new ArrayList<ArrayList<Integer>>();
     static int count = 0;
     static int N;
 
@@ -17,35 +20,35 @@ public class 경로_탐색_인접행렬 {
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        graph = new int[N + 1][N + 1];
-        check = new boolean[N + 1];
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken()); // 1
             int y = Integer.parseInt(st.nextToken()); // 2
-            graph[x][y] = 1; // graph[1][2] = 1
+            listGraph.get(x).add(y);
         }
-        DFS(1);
+
+        boolean[] check = new boolean[N + 1];
+        DFS(1, check);
         System.out.println(count);
 
     }
 
-    private static void DFS(int x) {
+    private static void DFS(int x, boolean[] check) {
 
         if (x == N) {
             count++;
-        }else {
+        } else {
             for (int i = 1; i <= N; i++) {
-                if (graph[x][i] == 1 && !check[i]) {
-                    check[x] = true;
-                    DFS(i);
-                    check[x] = false;
-                }
+
+//                if (graph[x][i] == 1 && !check[i]) {
+//                    check[x] = true;
+//                    DFS(i, check);
+//                    check[x] = false;
+//                }
+
             }
 
         }
-
 
 
     }
