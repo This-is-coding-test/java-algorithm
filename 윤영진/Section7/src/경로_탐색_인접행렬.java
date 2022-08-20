9e414224387bef7c220578a14e06869446ae1c6a
yoon <dudwls143@gmail.com>
2022-08-20 22:14:16 +0900

    경로 탐색 인접행렬 완료

---
+++ 윤영진/Section7/src/경로_탐색_인접행렬.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 경로_탐색_인접행렬 {
+
+    static int[][] graph;
+    static int count = 0;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        graph = new int[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()); // 1
+            int y = Integer.parseInt(st.nextToken()); // 2
+            graph[x][y] = 1; // graph[1][2] = 1
+        }
+        boolean[] check = new boolean[N + 1];
+//        check[1] = true;
+        DFS(1, check);
+        System.out.println(count);
+
+    }
+
+    private static void DFS(int x, boolean[] check) {
+
+        if (x == N) {
+            count++;
+        }else {
+            for (int i = 1; i <= N; i++) {
+
+                if (graph[x][i] == 1 && !check[i]) {
+                    check[x] = true;
+                    DFS(i, check);
+                    check[x] = false;
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
