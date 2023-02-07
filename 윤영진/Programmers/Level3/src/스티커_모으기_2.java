de6205685750280cce9f8d89f858d9eb3a50c63a
yoon-youngjin <dudwls143@gmail.com>
2023-02-07 21:32:09 +0900

    Pro 스티커 모으기(2)

---
+++ 윤영진/Programmers/Level3/src/스티커_모으기_2.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 스티커_모으기_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+    }
+}
