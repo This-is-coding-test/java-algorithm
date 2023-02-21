0bf5d0c0b36f1dd898a5e3c46cd7714d17dfb699
yoon-youngjin <dudwls143@gmail.com>
2023-02-21 19:46:52 +0900

    BOJ 숫자 카드

---
+++ 윤영진/BOJ/Binary-Search/src/숫자_카드.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 숫자_카드 {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while (M-- > 0) {
+            int f = Integer.parseInt(st.nextToken());
+
+            if (binarySearch(f)) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean binarySearch(int f) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == f) {
+                return true;
+            } else if (arr[mid] > f) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
