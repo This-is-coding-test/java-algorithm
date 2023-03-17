8a80258a76a0524845bdd540f58f0aeb03cbdbfb
yoon-youngjin <dudwls143@gmail.com>
2023-03-17 14:15:58 +0900

    Pro 기능개발

--- 윤영진/Programmers/Level2/src/기능개발.java
+++ 윤영진/Programmers/Level2/src/기능개발.java
@@ -7,37 +7,34 @@ public class 기능개발 {
 
     // 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] remain = new int[progresses.length];
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < progresses.length; i++) {
             int r = 100 - progresses[i];
             if (r % speeds[i] == 0) {
-                remain[i] = r / speeds[i];
+                queue.offer(r / speeds[i]);
             } else {
-                remain[i] = (r / speeds[i]) + 1;
+                queue.offer(r / speeds[i] + 1);
             }
         }
 
         List<Integer> results = new ArrayList<>();
 
-        int prev = remain[0];
+        int prev = queue.poll();
         int cnt = 1;
-        for (int i = 1; i < progresses.length; i++) {
-            if (prev >= remain[i]) {
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            if (prev >= curr) {
                 cnt++;
-            } else {
+            }
+            else {
                 results.add(cnt);
-                prev = remain[i];
+                prev = curr;
                 cnt = 1;
             }
         }
         results.add(cnt);
-
-        int[] answer = new int[results.size()];
-
-        for (int i = 0; i < results.size(); i++) {
-            answer[i] = results.get(i);
-        }
-
-        return answer;
+        return results.stream().mapToInt(i -> i).toArray();
     }
 }
