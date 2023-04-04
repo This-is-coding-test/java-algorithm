295459c56e81a52d1b1d292b2122b6b552770463
yoon-youngjin <dudwls143@gmail.com>
2023-04-04 14:26:53 +0900

    4. 미로의 최단거리 통로 완료

---
+++ 윤영진/practice/bfs/src/미로의_최단거리_통로_2.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class 미로의_최단거리_통로_2 {
+    static class Point {
+        int x;
+        int y;
+        int cnt;
+
+        public Point(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+
+    int[][] Board;
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+
+    public int solution(int[][] board) {
+        Board = board;
+
+        return bfs();
+    }
+
+    private int bfs() {
+        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[7][7];
+        queue.offer(new Point(0, 0, 0));
+        visited[0][0] = true;
+
+        int L = 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            L++;
+            for(int i = 0 ;i < len; i++) {
+                Point p = queue.poll();
+                if (p.x == 6 && p.y == 6) return p.cnt;
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = p.x + dx[d];
+                    int ny = p.y + dy[d];
+                    if (nx >= 0 && ny >= 0 && nx < 7 && ny < 7 && !visited[nx][ny] && Board[nx][ny] != 1) {
+                        visited[nx][ny] = true;
+                        queue.offer(new Point(nx, ny, L));
+                    }
+                }
+            }
+
+        }
+
+        return -1;
+
+
+    }
+
+    public static void main(String[] args) {
+        미로의_최단거리_통로_2 T = new 미로의_최단거리_통로_2();
+        int[][] arr = {{0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {1, 1, 0, 1, 0, 1, 1},
+                {1, 1, 0, 1, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0}};
+        System.out.println(T.solution(arr));
+
+        int[][] arr2 = {{0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1, 1, 1},
+                {1, 1, 0, 1, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 1, 0, 1, 0, 0}};
+        System.out.println(T.solution(arr2));
+    }
+}
