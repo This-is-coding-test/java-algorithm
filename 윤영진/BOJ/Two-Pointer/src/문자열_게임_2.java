fea6662825933fd627ece75b0e5808212a81f8fc
yoon-youngjin <dudwls143@gmail.com>
2023-04-19 22:31:36 +0900

    BOJ 문자열 게임 2

---
+++ 윤영진/BOJ/Two-Pointer/src/문자열_게임_2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 문자열_게임_2 {
+    // 알파벳 소문자로 이루어진 문자열 W
+    // 양의 정수 K
+    // 어떤 문자를 정확히 K개를 포함하는 가장 짧은 연속 문자열 길이
+    // 어떤 문자를 정확히 K개를 포함하고, 문자열의 첫 번째와 마지막 글자가 해당 문자로 같은 가장 긴 연속 문자열 길이
+
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String W = br.readLine();
+            int K = Integer.parseInt(br.readLine());
+
+            
+
+
+
+        }
+    }
+}
