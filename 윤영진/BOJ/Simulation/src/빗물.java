9e9a5e1cf82807b9a64982a66ba808d2bfb61bdb
yoon-youngjin <dudwls143@gmail.com>
2023-04-21 21:02:39 +0900

    BOJ 빗물

---
+++ 윤영진/BOJ/Simulation/src/빗물.java
@@ -0,0 +1,10 @@
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 빗물 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+    }
+}
