eed2b98117c992ce4524b9774a373bd52db0d728
yoon-youngjin <dudwls143@gmail.com>
2023-05-08 15:48:59 +0900

    BOJ 문서 검색

--- 윤영진/BOJ/String/src/문서_검색.java
+++ 윤영진/BOJ/String/src/문서_검색.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class 문서_찾기 {
+class 문서_검색 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
