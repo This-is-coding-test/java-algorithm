83773f4edddc4dbf814f9e426f2a3bcc4a878698
yoon-youngjin <dudwls143@gmail.com>
2023-06-05 22:28:38 +0900

    이코테 여행 계획

--- 윤영진/this-is-coding-test/graph/src/여행_계획.java
+++ 윤영진/this-is-coding-test/graph/src/여행_계획.java
@@ -6,8 +6,9 @@ import java.util.StringTokenizer;
 public class 여행_계획 {
     static int N, M;
     static final int INF = 987654321;
-//    static int[][] dist;
+    //    static int[][] dist;
     static int[][] dist;
+    static int[] unf;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,43 +17,56 @@ public class 여행_계획 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-//        dist = new int[N][N];
-        dist = new int[N][N];
+        unf = new int[N];
+        for (int i = 0; i < N; i++) {
+            unf[i] = i;
+        }
 
         for (int i = 0; i < N; i++) {
+            int p1 = i;
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                dist[i][j] = Integer.parseInt(st.nextToken());
-                if (dist[i][j] == 0) dist[i][j] = INF;
-            }
-        }
-//        for (int i = 0; i < N; i++) {
-//            Arrays.fill(dist[i], INF);
-//            dist[i][i] = 0;
-//        }
-
-        for (int middle = 0; middle < N; middle++) {
-            for (int start = 0; start < N; start++) {
-                for (int end = 0; end < N; end++) {
-                    dist[start][end] = Math.min(dist[start][end], dist[start][middle] + dist[middle][end]);
+                int p2 = Integer.parseInt(st.nextToken());
+                if (p2 == 1) {
+                    Union(p1, p2);
                 }
             }
         }
-
+        for (int i = 0; i < N; i++) {
+            System.out.print(unf[i] + " ");
+        }
 
         st = new StringTokenizer(br.readLine());
         int now = Integer.parseInt(st.nextToken());
         for (int i = 1; i < M; i++) {
             int next = Integer.parseInt(st.nextToken());
-            if (dist[now][next] != INF) {
-                now = next;
-            } else {
+
+            int f1 = Find(now);
+            int f2 = Find(next);
+            if (f1 != f2) {
                 System.out.println("NO");
                 return;
             }
+            now = next;
         }
 
         System.out.println("YES");
+
+    }
+
+    private static void Union(int p1, int p2) {
+        int f1 = Find(p1);
+        int f2 = Find(p2);
+
+        if (f1 != f2) {
+            unf[f1] = f2;
+        }
+    }
+
+    private static int Find(int p) {
+
+        if (p == unf[p]) return unf[p];
+        else return unf[p] = Find(unf[p]);
 
     }
 }
